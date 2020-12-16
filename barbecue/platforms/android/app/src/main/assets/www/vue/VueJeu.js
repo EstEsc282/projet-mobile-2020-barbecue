1305efbd4c4744e5ba08ce8fd57f1a290971861e
Alexandre Roussel <alexandreroussel.fr@gmail.com>
2020-12-15 21:24:11 -0500

    ajout interface jeu

--- barbecue/platforms/android/app/src/main/assets/www/vue/VueJeu.js
+++ barbecue/platforms/android/app/src/main/assets/www/vue/VueJeu.js
@@ -1,11 +1,32 @@
 class VueJeu{
 
     constructor(){
-        this.html = document.getElementById("html-vue-jeu").innerHTML;     
+        this.html = document.getElementById("html-vue-jeu").innerHTML;
+    }
+
+    initialiserViandes(){
+        this.viandes = [];
+        this.viandes.add(new Viande("merguez"));
+        this.viandes.add(new Viande("cote-de-porc"));
+        this.viandes.add(new Viande("cote-de-porc"));
     }
 
     afficher(){
         document.getElementsByTagName("body")[0].innerHTML = this.html;
+
+        /* let listeViandesHTML = document.getElementById("liste-viandes");
+        const listeViandesInnerHTML = listeViandesHTML.innerHTML;
+        let listeViandesHTMLRemplacement = "";
+
+        for(var viande in this.viandes){
+            let listeViandesInnerHTMLRemplacement = listeViandesInnerHTML;
+
+            listeViandesInnerHTMLRemplacement = listeViandesHTMLRemplacement.replace("{viande.url}", "decoration/illustration/"+viande.type+".png");
+            listeViandesHTMLRemplacement += listeViandesInnerHTML;
+        }
+
+        listeViandesHTML.innerHTML = listeViandesHTMLRemplacement; */
+
 
         /*var script = document.createElement('script');           
         script.src =  "librairieGraphique/Jeu.js";
