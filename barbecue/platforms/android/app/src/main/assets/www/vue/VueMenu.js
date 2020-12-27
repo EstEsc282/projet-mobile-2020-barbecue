0ef134bbac44f2349cc31683b2a5e804abf505a1
EstEsc282 <estebanescudero282@gmail.com>
2020-12-27 03:04:18 +0100

    Swipe fonctionnel en javascript

--- barbecue/platforms/android/app/src/main/assets/www/vue/VueMenu.js
+++ barbecue/platforms/android/app/src/main/assets/www/vue/VueMenu.js
@@ -8,6 +8,17 @@ class VueMenu{
     afficher(){
         document.getElementsByTagName("body")[0].innerHTML = this.html;
 
+        var region = new ZingTouch.Region(document.getElementById('child'));
+        var target = document.getElementById('child');
+
+        region.bind(target, 'swipe', function(e){
+            console.log(e.detail.data[0].velocity);
+            console.log(e.detail.data[0].currentDirection);
+            if(e.detail.data[0].currentDirection <= 270 && e.detail.data[0].currentDirection >= 90 && e.detail.data[0].velocity <= 5 && e.detail.data[0].velocity >= 0.2){
+                window.location.hash='#liste-meilleur-scores';
+            }
+        });
+
         //let listeVelo = document.getElementById("liste-velo");
         //const listeVeloInnerHTML = listeVelo.innerHTML;
         //let listeVeloHTMLRemplacement = "";
